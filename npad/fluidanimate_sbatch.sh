92cd7452c7e73443d61dd85bde1bf88dcc1e8b67
alexfurtunatoifrn <30511739+alexfurtunatoifrn@users.noreply.github.com>
2017-08-09 12:11:30 -0300

    Ajuste nos scripts npads para alterar os tamnhos de problema

--- npad/fluidanimate_sbatch.sh
+++ npad/fluidanimate_sbatch.sh
@@ -2,7 +2,7 @@
 ## cluster, test, service
 #SBATCH -p cluster
 #SBATCH --job-name=FLUIDANIMATE
-#SBATCH --time=0-6:00
+#SBATCH --time=0-20:00
 #SBATCH --output=/home/afdafurtunato/superpc_out/fluidanimate-slurm-%j.out
 #SBATCH --error=/home/afdafurtunato/superpc_out/fluidanimate-slurm-%j.err
 #SBATCH --nodes=1
@@ -10,4 +10,4 @@
 #SBATCH --mail-user=alexfurtunato@gmail.com
 #SBATCH --mail-type=ALL
 
-python3 ../runparsecprocess.py -p fluidanimate -c gcc-hooks -i native_01:10 -r 10 1,2,4,8,16,32,64
+python3 ../runparsecprocess.py -p fluidanimate -c gcc-hooks -i native_01:10 -r 6 1,2,6,10,14,18,22,26,30
