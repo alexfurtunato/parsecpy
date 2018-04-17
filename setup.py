f9f1cb4893cb94a6edf1851a836ae543a18f32b2
Alex Furtunato <alex.furtunato@ifrn.edu.br>
2018-04-17 15:45:17 -0300

    New version with graphical legend

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='parsecpy',
-      version='0.9.6',
+      version='0.9.7',
       description='Parsec Benchmark interface tool',
       url='https://github.com/alexfurtunatoifrn/parsecpy',
       author='Alex Furtunato',
