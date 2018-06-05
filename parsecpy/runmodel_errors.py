4de30bc8190a9bb732f31401a2488ed8b9d56783
Alex Furtunato <alex.furtunato@ifrn.edu.br>
2018-06-05 20:03:33 -0300

    Resoled bugs on runmodel_error

--- parsecpy/runmodel_errors.py
+++ parsecpy/runmodel_errors.py
@@ -14,9 +14,6 @@
 
     optional arguments:
       -h, --help            show this help message and exit
-      -a ALGORITHM, --algorithm ['csa' or 'pso']
-                            Optimization algorithm to use on modelling
-                            process.
       -m MODELFILENAME, --modelfilename MODELFILENAME
                             Absolute path from model filename with PSO Model
                             parameters executed previously.
@@ -109,10 +106,6 @@ def argsparsevalidation():
     parser = argparse.ArgumentParser(description='Script to run pso '
                                                  'modelling to predict a'
                                                  'parsec application output')
-    parser.add_argument('-a', '--algorithm', required=True,
-                        choices=['csa', 'pso'],
-                        help='Optimization algorithm to use on modelling'
-                             'process.')
     parser.add_argument('-m', '--modelfilepath',
                         help='Absolute path from model filename with '
                              'PSO Model parameters executed previously.')
@@ -185,6 +178,7 @@ def main():
         print('\nAlgorithm Execution: ', k)
 
         samples_args = []
+        test_size = 1 - (k / samples_n)
         for i in repetitions:
             # xy_train_test = train_test_split(x_without_limits,
             #                                  y_without_limits,
@@ -192,7 +186,6 @@ def main():
             # print(' ** ', i, ' - samples lens: x=', len(xy_train_test[0]), ', y=', len(xy_train_test[2]))
             # x_sample = np.concatenate((x_limits, xy_train_test[0]), axis=0)
             # y_sample = np.concatenate((y_limits, xy_train_test[2]))
-            test_size = 1 - (k/samples_n)
             xy_train_test = train_test_split(y_measure_detach['x'],
                                              y_measure_detach['y'],
                                              test_size=test_size)
@@ -214,14 +207,19 @@ def main():
                     errors.append(i['error'])
                     sols.append(list(i['sol']))
                 computed_errors.append({'k': k+1,
+                                        'train_size': 1-test_size,
                                         'errors': errors,
                                         'sols': sols})
 
         endtime = time.time()
         print('  Execution time = %.2f seconds' % (endtime - starttime))
 
+    head = {'algorithm': config['algorithm'],
+            'modeldatapath': args.modelfilepath,
+            }
     print('\n\n***** Final Results *****\n')
 
+    print(head)
     for i in computed_errors:
         print('Iteration {0:2d}'.format(i['k']))
         print('  * Errors: {}'.format(i['errors']))
@@ -232,9 +230,9 @@ def main():
 
     filedate = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     pkgname = args.modelfilepath.split('_')[0]
-    filename = '%s_%serrors_%s.errordat' % (pkgname, args.algorithm, filedate)
+    filename = '%s_%serrors_%s.errordat' % (pkgname, config['algorithm'], filedate)
     with open(filename, 'w') as f:
-        json.dump(computed_errors, f, ensure_ascii=False)
+        json.dump({'head': head, 'errors': computed_errors}, f, ensure_ascii=False)
     print('Errors data saved on filename: %s' % filename)
 
     print('\n\n***** ALL DONE! *****\n')
