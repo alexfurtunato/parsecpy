7258c96fbac553d8c4650e297fff227a69c667dc
Alex Furtunato <alex.furtunato@ifrn.edu.br>
2018-08-16 22:37:34 -0300

    Threadcpu repetitions correction

--- parsecpy/runprocess.py
+++ parsecpy/runprocess.py
@@ -212,6 +212,7 @@ def main():
                         print("Error: Error on System Execution : ",
                               sys.exc_info())
     print(datarun)
+    print(datarun.config)
     print(datarun.times())
     print("\n\n***** Done! *****\n")
     if args.frequency:
