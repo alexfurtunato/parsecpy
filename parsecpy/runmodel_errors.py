4ea598cbebd2e2d193d9ba03c085f8f97770fa9c
Alex Furtunato <alex.furtunato@ifrn.edu.br>
2018-09-09 23:36:10 -0300

    Adjust on Model Run Errors

--- parsecpy/runmodel_errors.py
+++ parsecpy/runmodel_errors.py
@@ -261,8 +261,8 @@ def main():
     for name, m in model_results.items():
         print("Model Name: {}".format(name))
         for ts, d in m["data"].items():
-            print(' Train Size {0:2d}: Median of Error {}'.format(ts))
-            print('  * Errors: {}'.format(np.median(d['error'])))
+            print(' Train Size: {0:2d}'.format(ts))
+            print('  * Median of Errors: {}'.format(np.median(d['error'])))
 
         filedate = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
         pkgname = os.path.basename(config['parsecpydatafilepath']).split('_')[0]
